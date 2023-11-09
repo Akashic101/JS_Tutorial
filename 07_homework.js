4d27450cafa1f3bfac2fd44b806114994ea496b7
David Moll <david.moll@xapling.de>
2023-11-09 12:38:32 +0100

    Added homework

---
+++ 07_homework.js
@@ -0,0 +1,26 @@
+// 1: Erstelle ein leeres Array namens einkaufsliste.
+
+
+
+// 2: Schreibe eine Funktion namens produktHinzufuegen, die ein Produkt zur Einkaufsliste hinzufügt. 
+// Die Funktion sollte den Namen des Produkts als Parameter nehmen und das Produkt zum Array hinzufügen.
+
+
+
+// 3: Schreibe eine Funktion namens produktEntfernen, die ein Produkt von der Einkaufsliste entfernt. 
+// Die Funktion sollte den Namen des zu entfernenden Produkts als Parameter nehmen und das Produkt aus dem Array entfernen.
+
+
+
+// 4: Schreibe eine Funktion namens listeAusgeben, die alle Produkte in der Einkaufsliste ausgibt.
+// Verwende eine if-else-Anweisung, um zu überprüfen, ob die Einkaufsliste leer ist. Wenn sie leer ist, 
+// gib aus: "Die Einkaufsliste ist leer." Andernfalls gib aus: "Einkaufsliste: " und dann die Liste der Produkte.
+
+
+
+// 5: Nutze einen for-loop, um durch die Einkaufsliste zu iterieren und die Produkte auszugeben.
+
+
+
+// 6: Teste dein Programm, indem du Produkte hinzufügst, einige entfernst und die Einkaufsliste ausgibst.
+
